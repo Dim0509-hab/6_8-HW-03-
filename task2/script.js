492f150ce32edafe7b465c686ea26deaad807a55
Dim0509-hab <pripoy.kanifol@gmail.com>
2025-01-11 21:08:38 +0500

    Начало

---
+++ task2/script.js
@@ -0,0 +1,20 @@
+const consolelog = document.querySelector('#consoleLog');
+ 
+consoleLog.addEventListener('click', () => {
+    alert('Служит для вывода информации в консоль');
+   
+})
+
+const prompt = document.querySelector('#prompt');
+ 
+prompt.addEventListener('click', () => {
+    alert('Отображает диалоговое окно с запросом на ввод текста.');
+   
+})
+
+const alerr1 = document.querySelector('#alert');
+ 
+alerr1.addEventListener('click', () => {
+    alert('Показывает диалоговое окно с сообщением и кнопкой OK');
+   
+})
---
+++ task4/script.js
@@ -0,0 +1,6 @@
+
+const zamena = document.querySelector('a');
+
+zamena.addEventListener('click', () => [
+    zamena.textContent = prompt('Введите текст')
+])
