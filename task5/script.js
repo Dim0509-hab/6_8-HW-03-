5b017e9240dea90f8699e221fcdebccd66f5177f
Dim0509-hab <pripoy.kanifol@gmail.com>
2025-01-12 20:20:36 +0500

    + Задание "Перехват формы"

--- task5/script.js
+++ task5/script.js
@@ -1,10 +1,26 @@
+const dubll1 = document.querySelector('#duplicateField');
 
-const userText = document.querySelector('#myText').value;
+(function() {
+    document.querySelector('input').addEventListener('keydown', function(e) {
+      if (e.keyCode === 13) {
+        // Дублируем значение текстового поля, запуск кнопкой enter
+         dubll1.textContent = this.value;
+      }
+    });
+  })();
 
+  const button = document.querySelector('button');
 
-
-
+    button.addEventListener('mousedown', () => [
+    // Запись значения в консоль и стираем текстовый блок duplicateField
+    console.log(dubll1.textContent), dubll1.textContent = ''
+  ])
   
- 
+  let input = document.querySelector('input');
+    // И стираем значение в input, обратным ходом кнопки. Понимаю что можно было всех в верхней строке.
+  button.addEventListener('mouseup', () => [
+    input.value = ''
+  ])
+
 
 
