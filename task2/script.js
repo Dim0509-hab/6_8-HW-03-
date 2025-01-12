d193df73b09bce158aae541f346b6cc05167983b
Dim0509-hab <pripoy.kanifol@gmail.com>
2025-01-12 21:35:55 +0500

    Стилизация задания 5

--- task2/script.js
+++ task2/script.js
@@ -5,16 +5,17 @@ consoleLog.addEventListener('click', () => {
    
 })
 
-const prompt = document.querySelector('#prompt');
- 
-prompt.addEventListener('click', () => {
-    alert('Отображает диалоговое окно с запросом на ввод текста.');
-   
-})
 
 const alerr1 = document.querySelector('#alert');
  
 alerr1.addEventListener('click', () => {
-    alert('Показывает диалоговое окно с сообщением и кнопкой OK');
+    alert('Показывает диалоговое окно с сообщением и кнопкой OK или Закрыть');
+   
+})
+
+const prompt = document.querySelector('#prompt');
+ 
+prompt.addEventListener('click', () => {
+    alert('Отображает диалоговое окно с запросом на ввод текста.');
    
 })
