6d456165d37eca199095917dfca0f9a74c7a0313
Dim0509-hab <pripoy.kanifol@gmail.com>
2025-01-14 00:53:49 +0500

    И опять Сложный Светофор

--- task3/script.js
+++ task3/script.js
@@ -1,25 +1,34 @@
-const trafficLightEl = document.querySelector('#trafficLight1');
+const trafficLightEl1 = document.querySelector('#trafficLight1');
+const trafficLightEl2 = document.querySelector('#trafficLight2');
+const trafficLightEl3 = document.querySelector('#trafficLight3');
 
-trafficLightEl.addEventListener('click', makeGreen);
+window.addEventListener('click', makeGreen);
 
 function makeGreen() {
-    trafficLightEl.style.background = ('green');
-    
-    trafficLightEl.addEventListener('click', makeYellow);
-    trafficLightEl.removeEventListener('click', makeGreen);
+
+    trafficLightEl1.style.background = ('green');
+    trafficLightEl2.style.background = ('black');
+    trafficLightEl3.style.background = ('black');
+    window.addEventListener('click', makeYellow);
+    window.removeEventListener('click', makeGreen);
         
 }
 
 function makeYellow() {
-    trafficLightEl.style.background = ('yellow');
-    trafficLightEl.addEventListener('click', makeRed);
-    trafficLightEl.removeEventListener('click', makeYellow);
+
+    trafficLightEl2.style.background = ('yellow');
+    trafficLightEl1.style.background = ('black');
+    trafficLightEl3.style.background = ('black');
+    window.addEventListener('click', makeRed);
+    window.removeEventListener('click', makeYellow);
        
 }
 
 function makeRed() {
-    trafficLightEl.style.background = ('red');
-    
-    trafficLightEl.addEventListener('click', makeGreen);
-    trafficLightEl.removeEventListener('click', makeRed);
-}     
+
+    trafficLightEl3.style.background = ('red');
+    trafficLightEl1.style.background = ('black');
+    trafficLightEl2.style.background = ('black');
+    window.addEventListener('click', makeGreen);
+    window.removeEventListener('click', makeRed);
+}
